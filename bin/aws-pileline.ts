e2e26bfdbeb3045fbd2570d9bed73fc5297949a3
Nikita <nekit1917@gmail.com>
2022-02-15 11:55:58 +0200

    Refactored tests

--- bin/aws-pileline.ts
+++ bin/aws-pileline.ts
@@ -5,17 +5,17 @@ import { newPipelineStack } from '../lib/aws-pileline-stack';
 const app = new App();
 
 let PipelineProps = {
-    //actionName: "CodeStarConnectionsSource",
-    actionName: "CodeCommitSource",
-    connectionArn: "arn:aws:codestar-connections:us-east-2:666398651410:connection/ff28b735-d3b5-4d03-bf79-1fb378a81b4a",
-    owner: "Nikita1917s",
-    repo: "KeyUA-Pipeline-Prototype",
-    branch: "main",
-    projectName: "Updated--PipelineStackProject",
-    pipelineName: "Updated--FirstPipeline",
-    slackNotifications: true,
-    slackWorkspaceId: "TG1K2568N",
-    slackChannelId: "C031Z1KJH6C"
+    //Select right actionName: (CodeStarConnectionsSource / CodeCommitSource)
+    actionName: "<actionName>",
+    connectionArn: "<connectionArn>",
+    owner: "<owner>",
+    repo: "<repo>",
+    branch: "<branch>",
+    projectName: "<projectName>",
+    pipelineName: "<pipelineName>",
+    slackNotifications: true || false,
+    slackWorkspaceId: "<slackWorkspaceId>",
+    slackChannelId: "<slackChannelId>"
 };
 
 new newPipelineStack(app, 'New--PipelineStack', PipelineProps);
